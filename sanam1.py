bc76325bdba93b677b554891036b2c1e898b09ba
23-hu <61133085+23-hu@users.noreply.github.com>
2020-02-17 12:08:14 +0530

    Add files via upload

---
+++ sanam1.py
@@ -0,0 +1,11 @@
+import random
+def ran():
+    print(random.randint(1,9))
+i=1    
+while (i<=5):
+    a=int(input("enter the value"))
+    if(a==ran()):
+          print("success")
+    else:
+          print("thank you")
+          i+=1
